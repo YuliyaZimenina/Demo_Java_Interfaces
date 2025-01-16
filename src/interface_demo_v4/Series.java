330e8f39bf6aade485389383bc679009acf6c32f
Yuliya Zimenina <yuliya.zimenina@gmail.com>
2025-01-16 16:52:56 +0100

    Demonstration of the use of a private interface method.

--- src/interface_demo_v4/Series.java
+++ src/interface_demo_v4/Series.java
@@ -2,6 +2,10 @@ package interface_demo_v4;
 
 // Interface for a class that generates a sequence of numbers
 // Adding a standard method ( getNextArray() ) to the interface
+/*
+Adding standard methods to an interface that use
+the private method (getArray () )
+ */
 public interface Series {
     // Method to return the next number in a sequence
     int getNext();
@@ -12,9 +16,28 @@ public interface Series {
     current element.
     */
     default int[] getNextArray(int n) {
+        return getArray(n);
+    }
+
+    /*
+    A method that returns an array containing the
+    next n elements in a sequence after skipping
+    elements
+     */
+    default int[] skipAndGetNextArray(int skip, int n) {
+        // Function to skip a specified number of elements
+        getArray(skip);
+        return getArray(n);
+    }
+
+    /*
+    A private method that returns an array
+    containing the following n elements
+     */
+    private int [] getArray(int n){
         int [] values = new int[n];
         for (int i = 0; i < n; i++) values[i] = getNext();
-        return values;
+            return values;
     }
 
     // Method for reset
